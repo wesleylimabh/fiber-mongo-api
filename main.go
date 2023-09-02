3dd33bd724cbf37be195944183715dd0987dfd87
Wesley Lima <wesleylima@gmx.com>
2023-09-02 16:20:27 -0300

    minimum fiber app

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"data": "Hello from Fiber and MongoDB"})
+	})
+
+	app.Listen(":3000")
+
+}
